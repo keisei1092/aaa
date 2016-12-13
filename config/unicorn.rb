dbdb4bc27a1dac4313ca48873b7495c1ed8deab2
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-12-13 18:14:47 +0900

    improve configuration file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,22 @@
-listen '/tmp/aaa.sock'
-pid '/tmp/aaa.pid'
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/home/aaa/aaa"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/tmp/aaa.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+stdout_path "/home/aaa/aaa/log/unicorn_stdout.log"
+stderr_path "/home/aaa/aaa/log/unicorn_stderr.log"
+
+# Unicorn socket
+listen "/tmp/aaa.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
